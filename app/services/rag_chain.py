cd61e75847123e8dbcbb164984b3de2e4bac15da
farihaNaqvi <fariha123.fariha123@gmail.com>
2026-01-15 23:11:54 +0530

    feat: complete FastAPI integration and resolve langchain-classic imports

--- app/services/rag_chain.py
+++ app/services/rag_chain.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 
